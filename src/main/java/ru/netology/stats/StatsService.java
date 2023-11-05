d7926689bb26465566c3d34b8a72d4a122454a77
ZhannaEr <eruntalle@gmail.com>
2023-11-05 19:15:42 +0300

    Files added

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,67 @@
+package ru.netology.stats;
+
+public class StatsService {
+    public long sum(long[] sales) {
+        long totalSales = 0;
+        for (long sale : sales) {
+            totalSales += sale;
+        }
+        return totalSales;
+
+    }
+
+    public long average(long[] sales) {
+        return sum(sales) / sales.length;
+    }
+
+    public int monthMaxSales(long[] sales) {
+        int monthMax = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[monthMax]) {
+                monthMax = i;
+            }
+
+        }
+        return monthMax + 1;
+    }
+
+    public int monthMinSales(long[] sales) {
+        int monthMin = 0;
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[monthMin]) {
+                monthMin = i;
+            }
+
+        }
+
+        return monthMin + 1;
+    }
+
+    public int bellowAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale < average(sales)) {
+                counter++;
+            }
+
+        }
+        return counter;
+
+    }
+
+    public int aboveAverage(long[] sales) {
+        int counter = 0;
+        long averageSale = average(sales);
+        for (long sale : sales) {
+            if (sale > average(sales)) {
+                counter++;
+            }
+
+        }
+        return counter;
+    }
+
+
+}
+
---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,83 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsServiceTest {
+
+    @Test
+    public void testSum() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = (8 + 15 + 13 + 15 + 17 + 20 + 19 + 20 + 7 + 14 + 14 + 18) / 12;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testmonthMaxSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthMaxSales(sales);
+        long expected = 8;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void testmonthMinSales() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.monthMinSales(sales);
+        long expected = 9;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void bellowAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.bellowAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void aboveAverage() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.aboveAverage(sales);
+        long expected = 5;
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+
+}
